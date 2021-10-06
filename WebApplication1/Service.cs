9f5b6f7637ed94b7f3b9054895dc663c2a21c4c2
Morody <77924462+Morody@users.noreply.github.com>
2021-10-06 21:39:41 +0300

    Add files via upload

---
+++ WebApplication1/Service.cs
@@ -0,0 +1,35 @@
+﻿using MailKit.Net.Smtp;
+using MimeKit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1
+{
+    public class EmailService
+    {
+        public async Task SendEmailAsync(string email, string subject, string message)
+        {
+            var emailMessage = new MimeMessage();
+
+            emailMessage.From.Add(new MailboxAddress("Администрация", "mail.ru"));
+            emailMessage.To.Add(new MailboxAddress("somemail.ru", email));
+            emailMessage.Subject = subject;
+            emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html)
+            {
+                Text = message
+            };
+
+            using (var client = new SmtpClient())
+            {
+                await client.ConnectAsync("smtp.mail.com", 465, true);
+                await client.AuthenticateAsync("morodymax@mail.ru", "password");
+                await client.SendAsync(emailMessage);
+
+                await client.DisconnectAsync(true);
+            }
+        }
+
+    }
+}
